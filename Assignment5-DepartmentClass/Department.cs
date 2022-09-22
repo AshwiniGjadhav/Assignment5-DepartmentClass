338b96aed9d58b4e4163de4bc553beb4fd39ae3e
Ashwini <Ashwini@LAPTOP-FQL7UM83>
2022-09-22 23:12:36 +0530

    Add project files.

---
+++ Assignment5-DepartmentClass/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5_DepartmentClass
+{
+ public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+
+        public override string ToString()
+        {
+            return $"  Department Details: {Id} {Name} {Location}";
+        }
+    }
+}
---
+++ Assignment5-DepartmentClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5_DepartmentClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Department[] department =
+            {
+           new Department {Id=14,Name="HR",Location="PUNE" },
+            new Department {Id=17,Name="devlopment",Location="mumbai" },
+             new Department{Id=19,Name="Banking",Location="pune" },
+             new Department{Id=13,Name="Technical",Location="delhi" },
+             new Department{Id=23,Name="HR",Location="Banglore" },
+            };
+
+            Console.WriteLine("\n");
+            foreach (Department d in department)
+            {
+                Console.WriteLine(d);
+            }
+        }
+    }
+}
